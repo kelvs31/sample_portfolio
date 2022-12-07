5b1906ab38c327eb42eb5782373b091873787e2a
kelvs31 <86040835+kelvs31@users.noreply.github.com>
2022-12-07 22:45:25 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,181 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, maximum-scale=1">
+<title>Kelvin Sumampong Portfolio</title>
+<link rel="icon" href="img/k.jpg" type="image/jpg">
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css"> 
+<link href="css/font-awesome.css" rel="stylesheet" type="text/css"> 
+<link href="css/animate.css" rel="stylesheet" type="text/css"> 
+</head>
+<body>
+
+<!--Header_section-->
+<header id="header_wrapper">
+  <div class="container">
+    <div class="header_box">
+	  <nav class="navbar navbar-inverse" role="navigation">
+      <div class="navbar-header">
+        <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        </div>
+	    <div id="main-nav" class="collapse navbar-collapse navStyle">
+			<ul class="nav navbar-nav" id="mainNav">
+			  <li class="active"><a href="#hero_section" class="scroll-link">Home</a></li>
+			  <li><a href="#service" class="scroll-link">Skills</a></li>
+			  <li><a href="#aboutUs" class="scroll-link">About</a></li>
+			  <li><a href="#clients" class="scroll-link">Experience/seminars</a></li>
+			</ul>
+      </div>
+	 </nav>
+    </div>
+  </div>
+</header>
+<!--Header_section--> 
+
+<!--Hero_Section-->
+<section id="hero_section" class="top_cont_outer">
+  <div class="hero_wrapper">
+    <div class="container">
+      <div class="hero_section">
+        <div class="row">
+          <div class="col-md-12">
+		   
+            <div class="top_left_cont zoomIn wow animated"> 
+              <h2 style="color: white">MY<br> <strong>PORTFOLIO</strong></h2>
+
+                <div class="underline"></div>
+             
+              <a href="#service" class="read_more2">Find out more</a> </div>
+          </div> 
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--Hero_Section--> 
+
+<!--Service-->
+<section  id="service">
+  <div class="container">
+    <h2>SKILLS</h2>
+    <div class="service_wrapper">
+      <div class="row">
+        <div class="col-lg-4">
+		<div class="service_icon delay-03s animated wow  zoomIn"> 
+		<span><img style="height: 100%; width: 100%;" src="img/1.png" alt="" /> </span> </div>
+          <div class="service_block">
+            
+            <h3 class="animated fadeInUp wow">Programming Skills</h3>
+            <p class="animated fadeInDown wow">I have basic knowledge in using html,css,jquery,php, and mysql. </p>
+          </div>
+        </div>
+        <div class="col-lg-4"> 
+		<div class="service_icon icon2  delay-03s animated wow zoomIn"> 
+		<span><img style="height: 100%; width: 100%;" src="img/2.gif" alt="" /> </span> </div> 
+		<div class="service_block">
+            <h3 class="animated fadeInUp wow">Communication Skills</h3>
+            <p class="animated fadeInDown wow"> I am good in communication.I can communicate interpersonal and intrapersonal.  </p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+		<div class="service_icon icon3  delay-03s animated wow zoomIn"> 
+		<span><img src="img/3.jpg" alt="" /> </span> </div>
+          <div class="service_block">
+            
+            <h3 class="animated fadeInUp wow">Others</h3>
+            <p class="animated fadeInDown wow">I am also computer literate and know how to use various microsoft office applications.  </p>
+          </div>
+        </div>
+      </div> 
+    </div>
+  </div>
+</section>
+<!--Service-->
+
+<section id="aboutUs" style="background-color: #CFE9EF;"><!--Aboutus-->
+<div class="inner_wrapper aboutUs-container fadeInLeft animated wow">
+  <div class="container"><br><br>
+    <h2>About Me</h2>
+    <div style="text-align: center;"><img style="height: 300px; width: 300px; border-radius: 50%;" src="img/pic.jpg"></div>
+    <div class="inner_section">
+ 
+	  <div class="row">
+						<div class="col-lg-12 about-us" style="font-size: 12pt;">
+							<div class="row">
+							<div class="col-md-6">
+								<h3>Profile</h3>
+									<p>
+										 HI! I am <b>Kelvin Sumampong</b>, 21 years old. I am currently a graduating student at the Polytechnic University of the Philippines (PUP), Institute of Technology under the program of Diploma in Information Communication Technology (DICT).
+									</p>
+									<p>
+										This portfolio served as a requirment and contains my accomplishment, trainings and seminars I attended (located at the experience/seminars tab). <br/> <br/>							
+									</p>
+
+								
+								</div><!-- /.col-md-6 -->
+								<div class="col-md-6">
+									<h3>Goals</h3>
+									<p>
+										Here are the things that I want to achieve in life.
+									</p>
+
+									<ul class="about-us-list">
+										<li class="points">To finish my study and to graduate with a degree.</li>
+										<li class="points">Aspiring to be a web developer someday.</li>
+										<li class="points">To get a stable job.</li>
+										<li class="points">To apply my skills and knowldege in workplace.</li>
+										<li class="points">To travel around the world.</li>
+									</ul><!-- /.about-us-list -->
+									 
+								</div><!-- /.col-md-6 -->
+								
+							</div><!-- /.row -->	
+						</div><!-- /.col-lg-12 -->
+					</div>
+      
+    </div>
+  </div> 
+  </div>
+</section>
+<!--Aboutus--> 
+
+
+<section class="page_section" id="clients" style="background-color: #70C4C0d;"><!--page_section-->
+  <br><h2>experience/seminars attended</h2><br>
+<!--page_section-->
+<div class="client_logos"><!--client_logos-->
+  <div class="container">
+    <ul class="fadeInRight animated wow" style="font-size: 15pt;">
+      <li><a href="img/s10.png" target="popup" onclick="window.open('img/s10.png','popup','width=600,height=600'); return false;">“Web Development LII”</a></li><br>
+      <li><a href="img/s_1.png" target="popup" onclick="window.open('img/s_1.png','popup','width=600,height=600'); return false;">"How I become a Front-End developer at 15 in the middle of a pandemic"</a></li><br>
+      <li><a href="img/s_2.png" target="popup" onclick="window.open('img/s_2.png','popup','width=600,height=600'); return false;">“CLOUD: ACCELERATING DIGITAL TRANSFORMATION”</a></li><br>
+      <li><a href="img/s3.png" target="popup" onclick="window.open('img/s3.png','popup','width=600,height=600'); return false;">“Cyber Security”</a></li><br>
+      <li><a href="img/s4.png" target="popup" onclick="window.open('img/s4.png','popup','width=600,height=600'); return false;">"Introduction to Algorithms, Data Structures, & Design Patterns for Aspiring Developers"</a></li><br>
+      <li><a href="img/s5.png" target="popup" onclick="window.open('img/s5.png','popup','width=600,height=600'); return false;">“CCNA Introduction and Technical Seminar”</a></li><br>
+      <li><a href="img/s6.png" target="popup" onclick="window.open('img/s6.png','popup','width=600,height=600'); return false;">“Game Development”</a></li><br>
+      <li><a href="img/s7.png" target="popup" onclick="window.open('img/s7.png','popup','width=600,height=600'); return false;">"JavaScript HTML DOM (Create Webpages using JavaScript)"</a></li><br>
+      <li><a href="img/s8.png" target="popup" onclick="window.open('img/s8.png','popup','width=600,height=600'); return false;">“Receiving and Responding to Workplace Communication”</a></li><br>
+      <li><a href="img/s9.png" target="popup" onclick="window.open('img/s9.png','popup','width=600,height=600'); return false;">“Participating in Workplace Communication”</a></li><br>
+    </ul>
+  </div>
+</div>
+</section>
+<!--client_logos-->
+
+<!--Footer-->
+<footer class="footer_wrapper" id="contact">
+  <div class="container">
+    <div class="footer_bottom"><span style="font-size: 15pt; color: #65D4EE;"> Kelvin Sumampong</span> </div>
+  </div>
+</footer>
+
+<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery-scrolltofixed.js"></script>
+<script type="text/javascript" src="js/jquery.nav.js"></script> 
+<script type="text/javascript" src="js/wow.js"></script> 
+<script type="text/javascript" src="js/custom.js"></script>
+
+</body>
+</html>
